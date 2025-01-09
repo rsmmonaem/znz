3cfa5e76b182cbeccf74ce5d215a3555cbdfed2f
mdabdullajobayer <mdabdullajovayer@gmail.com>
2025-01-09 11:21:30 +0600

    Section For Added Department

--- resources/views/section/_form.blade.php
+++ resources/views/section/_form.blade.php
@@ -1,4 +1,13 @@
-
+               @php $department = \App\Department::all(); @endphp
+			   <div class="form-group">
+					<label for="department">Department Name</label>
+					<select class="form-control" name="department_id" id="department_id">
+						<option value="">Select</option>
+						@foreach ($department as $d)
+							<option value="{{ $d->id }}" {{isset($b->department_id) ? ($b->department_id == $d->id ? 'selected' : '') : ''}}>{{ $d->name }}</option>
+						@endforeach
+					</select>
+				</div>
 			   <div class="form-group">
 				   {!! Form::label('name',trans('messages.name'),[])!!}
 				   {!! Form::input('text','name',isset($b->name) ? $b->name : '',['class'=>'form-control','placeholder'=>trans('messages.name')])!!}
