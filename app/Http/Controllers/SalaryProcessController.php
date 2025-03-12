404f762b5c1773ee122cef2d55f0cdf3179b4183
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2025-03-12 12:52:40 +0600

    Update SalaryProcessController.php

--- app/Http/Controllers/SalaryProcessController.php
+++ app/Http/Controllers/SalaryProcessController.php
@@ -221,21 +221,21 @@ class SalaryProcessController extends Controller
         $deductionsData = collect($deductionsData);
         $deductionsData = $deductionsData->unique('salary_type_id');
 
-        $advanceSalary = DB::table('salary_advance')
-        ->leftJoin('salary_advance_months', 'salary_advance.id', '=', 'salary_advance_months.salary_advance_id')
-        ->where('employeeId', $employeeId)
-        ->where('salary_advance.effectiveDate', '<', $toDate)
-        ->select('salary_advance.grossValue', 'salary_advance.grossOption', 'salary_advance_months.month', 'salary_advance_months.amount')
-        ->get();
-    
-    $monthNumber = (int)date('m', strtotime($toDate));
-    $advanceAmount = 0;
-    
-    foreach ($advanceSalary as $record) {
-        if ($record->month === $monthNumber) {
-            $advanceAmount = $record->amount;
+            $advanceSalary = DB::table('salary_advance')
+            ->leftJoin('salary_advance_months', 'salary_advance.id', '=', 'salary_advance_months.salary_advance_id')
+            ->where('employeeId', $employeeId)
+            ->where('salary_advance.effectiveDate', '<', $toDate)
+            ->select('salary_advance.grossValue', 'salary_advance.grossOption', 'salary_advance_months.month', 'salary_advance_months.amount')
+            ->get();
+        
+        $monthNumber = (int)date('m', strtotime($toDate));
+        $advanceAmount = 0;
+        
+        foreach ($advanceSalary as $record) {
+            if ($record->month == $monthNumber) {
+                $advanceAmount = $record->month; 
+            }
         }
-    }
 
         //$totalWorkedDays = $getTotalPresent + $holidays + $leave + $totalFridays + $spacial_holidays;
         $totalWorkedDays = $getTotalPresent;
