65383832f40bc5383d512340f99ae56a204fdf3f
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2025-03-18 14:01:06 +0600

    Update SalaryProcessController.php

--- app/Http/Controllers/SalaryProcessController.php
+++ app/Http/Controllers/SalaryProcessController.php
@@ -282,7 +282,7 @@ class SalaryProcessController extends Controller
         // $totalWorkedDays = $getTotalPresent + $holidays + $leave + $totalFridays + $spacial_holidays;
         // $totalWorkedDays = $getTotalPresent + $holidays + $leave + $totalFridays + $spacial_holidays;
         $totalWorkedDays = $getTotalPresent;
-        $totalAbsents = $TotalDays-$totalWorkedDays-$totalFridays-$spacial_holidays-$holidays-$leave;
+        $totalAbsents = $TotalDays-$totalWorkedDays-$totalFridays-$spacial_holidays-$holidays-$leave-$lwp;
         $perdaysAmount =  $salaryslab ? $salaryslab->gross / $TotalDays : 0;
         // $GrossAmountSalaryPerDays = $perdaysAmount * $totalWorkedDays;
         $GrossAmountSalaryPerDays = $perdaysAmount * $TotalDays;
