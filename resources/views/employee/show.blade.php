354d9b38eca8e2efc972873dd981bb3e121c8082
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2025-05-04 17:19:15 +0600

    up

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -218,10 +218,7 @@
 
 								{{-- Department --}}
 								<div class="col-sm-6">
-									{{-- @php
-									print_r($departments);
-									@endphp --}}
-									{{-- <div class="form-group flex-form-group">
+									<div class="form-group flex-form-group">
 										<select name="department_id" id="">
 											<option value="">Select One</option>
 											@foreach ($departments as $department)
@@ -230,7 +227,7 @@
 												{{ $department->name }}
 											</option>
 											@endforeach
-										</select> --}}
+										</select>
 
 										{{-- {!! Form::label('department_id',trans('messages.department'),['class' => 'control-label'])!!} --}}
 										{{-- {!! From::select('department_id',[null=>trans('messages.select_one')] + $department ,($employee->Profile->department_id) ? $employee->Profile->department_id : '',['class'=>'form-control input-xlarge select2me','placeholder'=>trans('messages.select_one')]) !!} --}}
