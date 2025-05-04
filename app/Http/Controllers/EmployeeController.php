86e0aab211a373033dfe3b70260b8fa7f052650a
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2025-05-04 17:36:31 +0600

    Update EmployeeController.php

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -227,7 +227,7 @@ class EmployeeController extends Controller{
         $document_types = DocumentType::pluck('name','id')->all();
 
         $templates = \App\Template::whereIsDefault(0)->pluck('name','id')->all();
-        $departments = Department::all()->pluck('name','id')->all();
+        $departments = Department::all();
         $assets = ['rte'];
         $menu = ['employee'];
         $type = DB::table('category')->get();
