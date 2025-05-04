0bc1e43990ecbd0bd3e285b8577a3d94ecbb4a4e
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2025-05-04 14:57:03 +0600

    Update index.blade.php

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -350,9 +350,9 @@ function populateEmployeeTable(data) {
     
     const mm = String(date.getMonth() + 1).padStart(2, '0');
     const dd = String(date.getDate()).padStart(2, '0');
-    const yy = String(date.getFullYear()).slice(-2);
-
-    return `${mm}-${dd}-${yy}`;
+    const yyyy = String(date.getFullYear()).padStart(4, '0');
+	
+    return `${mm}-${dd}-${yyyy}`;
 }
 
 tableBody.append(`
