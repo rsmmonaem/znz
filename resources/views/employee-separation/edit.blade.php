6477102334c0a5038d98ea9a89b43e39d433f551
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-23 18:48:49 +0600

    disabled-employee-sepe-edit

--- resources/views/employee-separation/edit.blade.php
+++ resources/views/employee-separation/edit.blade.php
@@ -44,7 +44,7 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="employeeId">Employee ID</label>
-                                    <select class="form-control select2me" id="employeeId" name="employeeId" readonly>
+                                    <select class="form-control select2me" id="employeeId" name="employeeId" disabled>
                                         <option value="">Select Employee ID</option>
                                         @foreach ($employee as $e)
                                             <option value="{{ $e->id }}"
