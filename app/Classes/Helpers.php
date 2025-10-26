05f1f8dc8cd4f86775bd99effee0f3561a41560c
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-26 17:08:31 +0600

    up

--- app/Classes/Helpers.php
+++ app/Classes/Helpers.php
@@ -13,7 +13,7 @@ class Helpers{
         ->select('users.first_name as employee_name', 'users.id','profile.employee_code')
         ->where('profile.branch_id', $branch_id)
         ->where('users.status', 'active')
-        ->orderBy('profile.employee_code', 'ASC')
+        ->orderByRaw('CAST(profile.employee_code AS UNSIGNED) ASC')
         ->get();
         return $employees;
     }
