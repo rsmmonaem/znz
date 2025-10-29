861f36f41e9df478294d4cc4e86f051c606d07ab
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-29 16:07:22 +0600

    FOOD JUNCTION LIMITED (UTTARA)  ---->  FOOD JUNCTION LIMITED (UTTARA)

--- app/Http/Controllers/EmpoloyeeCreate.php
+++ app/Http/Controllers/EmpoloyeeCreate.php
@@ -277,7 +277,7 @@ class EmpoloyeeCreate extends Controller
     public function migrate(Request $request)
     {
         try {
-            $oldUsers = DB::table('tbluser')->where('BranchName',"RTI")->get();
+            $oldUsers = DB::table('tbluser')->where('BranchName',"FOOD JUNCTION LIMITED (UTTARA)")->get();
 
             $migrated = 0;
             foreach ($oldUsers as $old) {
@@ -313,7 +313,7 @@ class EmpoloyeeCreate extends Controller
                 $profile->fathers_name = $old->FathersName;
                 $profile->mothers_name = $old->MothersName;
                 $profile->blood_group = $old->BloodGroup;
-                $profile->branch_id = 21;
+                $profile->branch_id = 57;
                 $profile->nid = $old->NID;
                 $profile->save();
 
@@ -343,7 +343,7 @@ class EmpoloyeeCreate extends Controller
 
             return response()->json([
                 'status' => 'success',
-                'message' => $migrated . "user(s) migrated successfully to Head Office"
+                'message' => $migrated . "user(s) migrated successfully to FOOD JUNCTION LIMITED (UTTARA)"
             ]);
 
         } catch (\Exception $e) {
