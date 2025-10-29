bba5491c2d37c79905234710cf5773e4b736e1f1
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-29 16:26:16 +0600

    KASUNDI RESTORA LIMITED, KHILKHET ----->  Kasundi Restora Ltd. (Khilkhet Branch)

--- app/Http/Controllers/EmpoloyeeCreate.php
+++ app/Http/Controllers/EmpoloyeeCreate.php
@@ -277,7 +277,7 @@ class EmpoloyeeCreate extends Controller
     public function migrate(Request $request)
     {
         try {
-            $oldUsers = DB::table('tbluser')->where('BranchName',"KASUNDI RESTORA LIMITED, UTTARA")->get();
+            $oldUsers = DB::table('tbluser')->where('BranchName',"KASUNDI RESTORA LIMITED, KHILKHET")->get();
 
             $migrated = 0;
             foreach ($oldUsers as $old) {
@@ -313,7 +313,7 @@ class EmpoloyeeCreate extends Controller
                 $profile->fathers_name = $old->FathersName;
                 $profile->mothers_name = $old->MothersName;
                 $profile->blood_group = $old->BloodGroup;
-                $profile->branch_id = 51;
+                $profile->branch_id = 19;
                 $profile->nid = $old->NID;
                 $profile->save();
 
@@ -343,7 +343,7 @@ class EmpoloyeeCreate extends Controller
 
             return response()->json([
                 'status' => 'success',
-                'message' => $migrated . "user(s) migrated successfully to KASUNDI RESTORA LIMITED, UTTARA"
+                'message' => $migrated . "user(s) migrated successfully to Kasundi Restora Ltd. (Khilkhet Branch)"
             ]);
 
         } catch (\Exception $e) {
