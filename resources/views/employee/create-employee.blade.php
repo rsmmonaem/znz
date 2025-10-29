14f9f8e5d9f115ad2b49295669e0bbc88e87f644
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-29 16:30:00 +0600

    migrate-done

--- resources/views/employee/create-employee.blade.php
+++ resources/views/employee/create-employee.blade.php
@@ -32,9 +32,6 @@
         }
     </style>
     <div class="row">
-        <div class="col-1">
-            <button id="migrateBtn" class="btn btn-primary">Start Migration</button>
-        </div>
         <div class="col-md-12">
             <div class="panel-group" id="accordion">
                 <!-- Personal Details -->
@@ -426,25 +423,6 @@
 @stop
 
 @section('javascript')
-<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-<script>
-    $('#migrateBtn').click(function(){
-        $.ajax({
-            url: '{{ url("migrate-users") }}',
-            type: 'POST',
-            data: { _token: '{{ csrf_token() }}' },
-            beforeSend: function() {
-                alert('Migration starting...');
-            },
-            success: function(res) {
-                alert(res.message);
-            },
-            error: function(xhr) {
-                alert('Error: ' + xhr.responseText);
-            }
-        });
-    });
-</script>
     <script>
         function getDesignations() {
             var department_id = $('#department_id').val();
