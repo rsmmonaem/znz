7a471d33b82f5af100700a6d4be4436ae4580058
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-29 15:31:52 +0600

    KASUNDI RESTORA LTD (ICDDRB)  ---> Kasundi Restora Ltd. (ICDDRB Branch)

--- app/Http/Controllers/EmpoloyeeCreate.php
+++ app/Http/Controllers/EmpoloyeeCreate.php
@@ -277,7 +277,7 @@ class EmpoloyeeCreate extends Controller
     public function migrate(Request $request)
     {
         try {
-            $oldUsers = DB::table('tbluser')->where('BranchName',"JUNE FOODS LTD, (UTTARA FACTORY)")->get();
+            $oldUsers = DB::table('tbluser')->where('BranchName',"KASUNDI RESTORA LTD (ICDDRB)")->get();
 
             $migrated = 0;
             foreach ($oldUsers as $old) {
@@ -313,7 +313,7 @@ class EmpoloyeeCreate extends Controller
                 $profile->fathers_name = $old->FathersName;
                 $profile->mothers_name = $old->MothersName;
                 $profile->blood_group = $old->BloodGroup;
-                $profile->branch_id = 53;
+                $profile->branch_id = 37;
                 $profile->nid = $old->NID;
                 $profile->save();
 
@@ -343,7 +343,7 @@ class EmpoloyeeCreate extends Controller
 
             return response()->json([
                 'status' => 'success',
-                'message' => $migrated . "user(s) migrated successfully to JUNE FOODS LTD, (UTTARA FACTORY)!"
+                'message' => $migrated . "user(s) migrated successfully to Kasundi Restora Ltd. (ICDDRB Branch)!"
             ]);
 
         } catch (\Exception $e) {
