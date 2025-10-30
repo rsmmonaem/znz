f241b1765215651351c904e58a839d27d66a8c44
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-30 17:12:45 +0600

    Kasundi Farmgate --->  KASUNDI RESTORA LIMITED, FARMGATE

--- app/Http/Controllers/EmpoloyeeCreate.php
+++ app/Http/Controllers/EmpoloyeeCreate.php
@@ -278,7 +278,7 @@ class EmpoloyeeCreate extends Controller
     public function migrate(Request $request)
     {
         try {
-            $oldUsers = DB::table('tbluser')->where("BranchName","Food Junction Ltd. Uttara")->get();
+            $oldUsers = DB::table('tbluser')->where("BranchName","Kasundi Farmgate")->get();
             
 
             $migrated = 0;
@@ -316,7 +316,7 @@ class EmpoloyeeCreate extends Controller
                 $profile->mothers_name = $old->MothersName;
                 $profile->blood_group = $old->BloodGroup;
                 $profile->nid = $old->NID;
-                $profile->branch_id = 57;
+                $profile->branch_id = 50;
                 $profile->save();
 
                 
@@ -345,7 +345,7 @@ class EmpoloyeeCreate extends Controller
 
             return response()->json([
                 'status' => 'success',
-                'message' => $migrated . ' user(s) migrated successfully to FOOD JUNCTION LIMITED (UTTARA)!'
+                'message' => $migrated . ' user(s) migrated successfully to KASUNDI RESTORA LIMITED, FARMGATE!'
             ]);
 
         } catch (\Exception $e) {
