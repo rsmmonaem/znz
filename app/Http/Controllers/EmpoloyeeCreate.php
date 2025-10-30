6bb7f24a48392dd0119f29f2a1acbebdac3e0c22
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-10-30 18:03:57 +0600

    No Branch --->  New Branch for Dev

--- app/Http/Controllers/EmpoloyeeCreate.php
+++ app/Http/Controllers/EmpoloyeeCreate.php
@@ -278,7 +278,7 @@ class EmpoloyeeCreate extends Controller
     public function migrate(Request $request)
     {
         try {
-            $oldUsers = DB::table('tbluser')->where("BranchName","BSN, Gazipur")->get();
+            $oldUsers = DB::table('tbluser')->get();
             
 
             $migrated = 0;
@@ -316,7 +316,7 @@ class EmpoloyeeCreate extends Controller
                 $profile->mothers_name = $old->MothersName;
                 $profile->blood_group = $old->BloodGroup;
                 $profile->nid = $old->NID;
-                $profile->branch_id = 28;
+                $profile->branch_id = 58;
                 $profile->save();
 
                 
@@ -345,7 +345,7 @@ class EmpoloyeeCreate extends Controller
 
             return response()->json([
                 'status' => 'success',
-                'message' => $migrated . ' user(s) migrated successfully to Kasundi Restora Ltd. (Bhaluka Project)!'
+                'message' => $migrated . ' user(s) migrated successfully to New Branch for Dev!'
             ]);
 
         } catch (\Exception $e) {
