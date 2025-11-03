2293814beb6a544e9876bcbd492e6f97d6aec09b
MOSHARAFhPARVEZ <parveznbh2026@gmail.com>
2025-11-03 11:10:47 +0600

    whd-date-to-week-name

--- resources/views/whd/_form.blade.php
+++ resources/views/whd/_form.blade.php
@@ -1,15 +1,15 @@
 <div class="form-group">
-    <labe>Group</label>
+    <label>Group</label>
     <select class="form-control" name="group_id" id="group_id">
         <option value="">Select Group</option>
         @foreach ($group as $g)
-            <option value="{{ $g->id }}" selected>{{ $g->name }}</option>
+            <option value="{{ $g->id }}">{{ $g->name }}</option>
         @endforeach
     </select>
 </div>
 
 <div class="form-group">
-    <label>Brach</label>
+    <label>Branch</label>
     <select class="form-control" name="branch_id" id="branch_id">
         <option value="">Select Branch</option>
         @foreach ($branch as $b)
@@ -19,9 +19,17 @@
 </div>
 
 <div class="form-group">
-    <label>Date</label>
-    <input type="text" name="date" id="date" readonly placeholder="{{ trans('messages.date')}}" class="form-control mdatepicker">
+    <label>Days</label><br>
+    @php
+        $days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    @endphp
+    @foreach ($days as $day)
+        <label style="margin-right: 10px;">
+            <input type="checkbox" name="days[]" value="{{ $day }}"> {{ $day }}
+        </label>
+    @endforeach
 </div>
+
 <div class="form-group">
     <label>Employee ID</label>
     <select class="form-control" name="employee_id" id="employee_id">
