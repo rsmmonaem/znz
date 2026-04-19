7eea3931bef4d6b9e107ce35ae2ea8aec930e377
Md Mosharaf Hossain <parveznbh2026@gmail.com>
2026-04-19 10:20:01 +0600

    up

--- app/Bank.php
+++ app/Bank.php
@@ -2,18 +2,19 @@
 namespace App;
 use Eloquent;
 
-class Bank extends Eloquent {
+class Bank extends Eloquent
+{
 
 	protected $fillable = [
-							'bank_name',
-							'account_name',
-							'account_number',
-							'branch',
-							'routing_number',
-							'swift_code',
-							'description',
-							'status'
-						];
+		'bank_name',
+		'account_name',
+		'account_number',
+		'branch',
+		'routing_number',
+		'swift_code',
+		'description',
+		'status'
+	];
 	protected $primaryKey = 'id';
 	protected $table = 'company_banks';
 }
