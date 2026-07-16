fb529eb6083563acf44f97cb352ff1c2873b427a
Rakibul Alam <rakibulalam59900@gmail.com>
2026-07-16 18:26:44 +0600

    ups

--- app/Http/Controllers/IdCardChecklistController.php
+++ app/Http/Controllers/IdCardChecklistController.php
@@ -16,7 +16,10 @@ class IdCardChecklistController extends Controller
         ->leftJoin('sections', 'profile.section_id', '=', 'sections.id')
         ->select('id_card.*', 'profile.employee_code','sections.name as section_name','branchs.name as branch_name','users.first_name', 'designations.name', 'departments.name as department_name');
         
-        $search = \Request::get('search');
+        if (isset($_GET['search'])) {
+            dd('Search Parameter Reached Controller:', $_GET['search']);
+        }
+        $search = isset($_GET['search']) ? $_GET['search'] : '';
         if (!empty($search)) {
             $query->where(function($q) use ($search) {
                 $q->where('profile.employee_code', 'like', "%{$search}%")
